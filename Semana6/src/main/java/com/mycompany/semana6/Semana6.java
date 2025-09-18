65f1ab82feeeb931a4b74374359875edcffaa1ba
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-09-17 23:07:07 -0500

    listo

--- Semana6/src/main/java/com/mycompany/semana6/Semana6.java
+++ Semana6/src/main/java/com/mycompany/semana6/Semana6.java
@@ -8,7 +8,7 @@ package com.mycompany.semana6;
  *
  * @author CIELO
  */
-public class Semana6 {
+public class SEMANA6 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");

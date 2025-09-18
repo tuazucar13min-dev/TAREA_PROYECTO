2485f70fb34e940b08865627cfe3d836dc32d630
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-09-17 22:49:47 -0500

    Semana_6

---
+++ Semana6/src/main/java/com/mycompany/semana6/Semana6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana6;
+
+/**
+ *
+ * @author CIELO
+ */
+public class Semana6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

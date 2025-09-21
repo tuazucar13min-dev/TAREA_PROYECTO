9dbb5e220eb193f83eb25f0c3797be97604abbda
tuazucar13min-dev <tuazucar13min@gmail.com>
2025-09-20 21:10:41 -0500

    avance

---
+++ SEMANA_7/src/main/java/com/mycompany/semana_7/SEMANA_7.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semana_7;
+
+/**
+ *
+ * @author CIELO
+ */
+public class SEMANA_7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
